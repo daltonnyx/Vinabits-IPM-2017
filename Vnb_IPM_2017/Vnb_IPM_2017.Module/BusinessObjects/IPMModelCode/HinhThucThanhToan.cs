2817b91640cbc79be999b68f79f53dc27757cdfb
aquez <aquez@AQUEZ-PC>
2017-07-25 11:34:52 +0700

    Add xaf default property

--- Vnb_IPM_2017/Vnb_IPM_2017.Module/BusinessObjects/IPMModelCode/HinhThucThanhToan.cs
+++ Vnb_IPM_2017/Vnb_IPM_2017.Module/BusinessObjects/IPMModelCode/HinhThucThanhToan.cs
@@ -3,9 +3,11 @@ using DevExpress.Xpo;
 using DevExpress.Data.Filtering;
 using System.Collections.Generic;
 using System.ComponentModel;
+using DevExpress.ExpressApp.DC;
+
 namespace Vnb_IPM_2017.Module.BusinessObjects
 {
-
+    [XafDefaultProperty("TieuDe")]
     public partial class HinhThucThanhToan
     {
         public HinhThucThanhToan(Session session) : base(session) { }
